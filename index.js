306647d6b613dd8646d4314b81f8c0ccfc3e45fc
♿ <biznes@2.pl>
2022-01-15 23:39:25 +0100

    r

--- index.js
+++ index.js
@@ -16,40 +16,47 @@ const algorithms = {
     HS512: { name: 'HMAC', hash: { name: 'SHA-512' } }
 }
 
+function _utf8ToUint8Array(str) {
+    return Base64URL.parse(btoa(unescape(encodeURIComponent(str))))
+}
+
+function _str2ab(str) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+function _decodePayload(raw) {
+    switch (raw.length % 4) {
+        case 0:
+            break
+        case 2:
+            raw += '=='
+            break
+        case 3:
+            raw += '='
+            break
+        default:
+            throw new Error('Illegal base64url string!')
+    }
+    try {
+        return JSON.parse(decodeURIComponent(escape(atob(raw))))
+    } catch {
+        return null
+    }
+}
+
+function decode(token) {
+    return _decodePayload(token.split('.')[1].replace(/-/g, '+').replace(/_/g, '/'))
+}
+
 class JWT {
     constructor() {
         if (!crypto || !crypto.subtle)
             throw new Error('Crypto not supported!')
-    }
-    _utf8ToUint8Array(str) {
-        return Base64URL.parse(btoa(unescape(encodeURIComponent(str))))
-    }
-    _str2ab(str) {
-        const buf = new ArrayBuffer(str.length);
-        const bufView = new Uint8Array(buf);
-        for (let i = 0, strLen = str.length; i < strLen; i++) {
-            bufView[i] = str.charCodeAt(i);
-        }
-        return buf;
-    }
-    _decodePayload(raw) {
-        switch (raw.length % 4) {
-            case 0:
-                break
-            case 2:
-                raw += '=='
-                break
-            case 3:
-                raw += '='
-                break
-            default:
-                throw new Error('Illegal base64url string!')
-        }
-        try {
-            return JSON.parse(decodeURIComponent(escape(atob(raw))))
-        } catch {
-            return null
-        }
     }
     async sign(payload, secret, options = { algorithm: 'HS256' }) {
         if (typeof options === 'string')
@@ -65,16 +72,16 @@ class JWT {
             throw new Error('algorithm not found')
         payload.iat = Math.floor(Date.now() / 1000)
         const payloadAsJSON = JSON.stringify(payload)
-        const partialToken = `${Base64URL.stringify(this._utf8ToUint8Array(JSON.stringify({ alg: options.algorithm, kid: options.keyid })))}.${Base64URL.stringify(this._utf8ToUint8Array(payloadAsJSON))}`
+        const partialToken = `${Base64URL.stringify(_utf8ToUint8Array(JSON.stringify({ alg: options.algorithm, kid: options.keyid })))}.${Base64URL.stringify(_utf8ToUint8Array(payloadAsJSON))}`
         let keyFormat = 'raw'
         let keyData
         if (secret.startsWith('-----BEGIN')) {
             keyFormat = 'pkcs8'
-            keyData = this._str2ab(atob(secret.replace(/-----BEGIN.*?-----/g, '').replace(/-----END.*?-----/g, '').replace(/\s/g, '')))
+            keyData = _str2ab(atob(secret.replace(/-----BEGIN.*?-----/g, '').replace(/-----END.*?-----/g, '').replace(/\s/g, '')))
         } else
-            keyData = this._utf8ToUint8Array(secret)
+            keyData = _utf8ToUint8Array(secret)
         const key = await crypto.subtle.importKey(keyFormat, keyData, importAlgorithm, false, ['sign'])
-        const signature = await crypto.subtle.sign(importAlgorithm, key, this._utf8ToUint8Array(partialToken))
+        const signature = await crypto.subtle.sign(importAlgorithm, key, _utf8ToUint8Array(partialToken))
         return `${partialToken}.${Base64URL.stringify(new Uint8Array(signature))}`
     }
     async verify(token, secret, options = { algorithm: 'HS256' }) {
@@ -92,7 +99,7 @@ class JWT {
         const importAlgorithm = algorithms[options.algorithm]
         if (!importAlgorithm)
             throw new Error('algorithm not found')
-        const payload = this.decode(token)
+        const payload = decode(token)
         if (payload.nbf && payload.nbf >= Math.floor(Date.now() / 1000))
             return false
         if (payload.exp && payload.exp < Math.floor(Date.now() / 1000))
@@ -101,15 +108,12 @@ class JWT {
         let keyData
         if (secret.startsWith('-----BEGIN')) {
             keyFormat = 'pkcs8'
-            keyData = this._str2ab(atob(secret.replace(/-----BEGIN.*?-----/g, '').replace(/-----END.*?-----/g, '').replace(/\s/g, '')))
+            keyData = _str2ab(atob(secret.replace(/-----BEGIN.*?-----/g, '').replace(/-----END.*?-----/g, '').replace(/\s/g, '')))
         } else
-            keyData = this._utf8ToUint8Array(secret)
+            keyData = _utf8ToUint8Array(secret)
         const key = await crypto.subtle.importKey(keyFormat, keyData, importAlgorithm, false, ['sign'])
-        const res = await crypto.subtle.sign(importAlgorithm, key, this._utf8ToUint8Array(tokenParts.slice(0, 2).join('.')))
+        const res = await crypto.subtle.sign(importAlgorithm, key, _utf8ToUint8Array(tokenParts.slice(0, 2).join('.')))
         return Base64URL.stringify(new Uint8Array(res)) === tokenParts[2]
-    }
-    decode(token) {
-        return this._decodePayload(token.split('.')[1].replace(/-/g, '+').replace(/_/g, '/'))
     }
 }
 
